e8bdee05ec0399d3ccbffc14f16355c037c742f0
Randy Ubillos <randy@mac.com>
2019-06-29 17:49:17 -0700

    Removed unused files

--- Duet-timelapse/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /
-cd /home/pi/Duet-timelapse
-sudo python3 timelapse.py '/home/pi/timelapse-movies' 'http://192.168.7.250' 'http://hypercam.local:8080/?action=snapshot'
-cd /
