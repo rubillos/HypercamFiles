66433e9ff70d8b1e04ecc56b77dfe20805bb82c5
Randy Ubillos <randy@mac.com>
2019-10-27 13:21:26 +0100

    Added baby-stepping and sounds

--- Duet-printmonitor/settings-example.py
+++ Duet-printmonitor/settings-example.py
@@ -3,20 +3,24 @@ printer_status_delay = 3.0
 minimum_image_count = 20
 initial_wait = 10
 
+button_down_pin = 12
+button_up_pin = 13
+baby_step_amount = 0.02
+
+sound_volume = 1.0
+
 duet_host = "192.168.7.250" # address of the Duet Wifi or Duet Ethernet
 
 snapshot_folder = "/home/pi/timelapse-movies" # folder to place final movies and temporary images into
+sounds_folder = "/home/pi/sounds"
 
 mjpg_streamer_folder = "/home/pi/installs/mjpg-streamer/mjpg-streamer-experimental" # location of mjpg_streamer
-
-# Options for mjpg-mjpg_streamer
-# In this case the camera is rotated 90º, quality is 95, white balance is set to tungsten
 start_mjpg_streamer = "sudo ./mjpg_streamer -o 'output_http.so -w ./www' -i 'input_raspicam.so -x 972 -y 1296 -fps 15 -quality 95 -awb tungsten -ev -1 -mm matrix -rot 90'"
-
 webcam_url = "http://127.0.0.1:8080/?action=snapshot" # url for retrieving images from mjpg_streamer
 
 create_movie = True
-encoding_options = "-c:v h264_omx -vf format=yuv420p -b:v 15000k" # use hardware encoding, max 15MB/s
+encoding_options = "-b:v 10M -minrate 3M -maxrate 15M -bufsize 15M -c:v h264_omx -vf format=yuv420p -preset slow -crf 18 -profile:v -bf 2 -coder 1 -threads 4 -cpu-used 0"
+
 
 # Array of z heights and crop factors (from the top of frame) to be processed using FFMPEG
 crop_factors = ((0, 0.23), (50, 0.45), (100, 0.74), (200, 0.82), (300, 1.0))
--- Duet-printmonitor/timelapse.py
+++ Duet-printmonitor/timelapse.py
@@ -5,6 +5,7 @@ import json
 import os
 import requests
 import socket
+import select
 import sys
 import textwrap
 import time
@@ -13,6 +14,7 @@ import json,urllib
 import time
 import shutil
 import RPi.GPIO as GPIO
+import pygame as pg
 
 from subprocess import Popen, PIPE
 from twilio.rest import Client
@@ -26,6 +28,8 @@ from email import encoders
 
 import PIL.Image as Image
 
+from enum import Enum
+
 from settings import *
 
 debugging = False
@@ -33,12 +37,19 @@ debugging = False
 twilio_client = None
 pwm_channel = None
 
+sock = None
+conn = None
+
+printing = False
+awaiting_baby_step = False
+
 if send_twilio_sms:
     twilio_client = Client(twilio_account_sid, twilio_auth_token)
 
 def log_print(*msg):
     print(msg)
     # print(current_time_string(), *msg, sys.stderr)
+
 
 class InterpolatedArray(object):
   def __init__(self, points):
@@ -56,7 +67,7 @@ class InterpolatedArray(object):
   def _GetBoundingPoints(self, x):
     lower_point = None
     upper_point = self.points[0]
-    for point  in self.points[1:]:
+    for point in self.points[1:]:
       lower_point = upper_point
       upper_point = point
       if x <= upper_point[0]:
@@ -66,6 +77,7 @@ class InterpolatedArray(object):
   def _Interpolate(self, x, lower_point, upper_point):
     slope = (float(upper_point[1] - lower_point[1]) / (upper_point[0] - lower_point[0]))
     return lower_point[1] + (slope * (x - lower_point[0]))
+
 
 class SimpleLineProtocol:
     def __init__(self, sock):
@@ -78,15 +90,20 @@ class SimpleLineProtocol:
         self.socket.sendall(msg.encode())
 
     def read_line(self):
-        while b'\n' not in self.buffer:
+        line = b''
+        readable, writable, exceptional = select.select([sock], [], [], 0.5)
+
+        if len(readable) > 0:
             d = self.socket.recv(1024)
             if not d:
                 raise socket.error()
             self.buffer = self.buffer + d
 
-        i = self.buffer.find(b'\n')
-        line = self.buffer[:i]
-        self.buffer = self.buffer[i:].lstrip()
+            if b'\n' in self.buffer:
+                i = self.buffer.find(b'\n')
+                line = self.buffer[:i]
+                self.buffer = self.buffer[i:].lstrip()
+
         return line
 
     def read_json_line(self):
@@ -96,8 +113,23 @@ class SimpleLineProtocol:
         json_data = json.loads(line[line.find(b'{'):].decode())
         return json_data
 
+    def read_data(self):
+        json_data = None
+        message = None
+
+        line = self.read_line()
+
+        if b'{' in line and b'}' in line:
+            json_data = json.loads(line[line.find(b'{'):].decode())
+        elif len(line) > 0:
+            message = line
+
+        return json_data,message
+
+
 def current_time_string():
     return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
 
 def layer_changed(timelapse_folder, webcam_url):
     r = requests.get(webcam_url)
@@ -120,6 +152,7 @@ def layer_changed(timelapse_folder, webcam_url):
 
         return ""
 
+
 def clear_pwm():
     global pwm_channel
 
@@ -127,12 +160,14 @@ def clear_pwm():
         pwm_channel.stop()
         pwm_channel = None
 
+
 def set_active_light(enabled):
     clear_pwm()
     if enabled:
         GPIO.output(led_pin, GPIO.HIGH)
     else:
         GPIO.output(led_pin, GPIO.LOW)
+
 
 def blink_error(waitTime):
     global pwm_channel
@@ -143,6 +178,7 @@ def blink_error(waitTime):
     time.sleep(waitTime)
     clear_pwm()
     set_active_light(False)
+
 
 def send_email(subject, msgText, picPath):
     if send_email:
@@ -165,21 +201,23 @@ def send_email(subject, msgText, picPath):
         server.sendmail(sender_email, receiver_email, msg.as_string())
         server.quit()
 
-def send_sms(msgBody):
-    if twilio_client:
-        twilio_client.messages.create(from_=twilio_from_number, to=twilio_to_number, body=msgBody)
 
-def make_a_movie(output_filename, source_folder, elapsed_time, last_picture_path, currentZ):
+def send_sms(msg_body):
+    if twilio_client:
+        twilio_client.messages.create(from_=twilio_from_number, to=twilio_to_number, body=msg_body)
+
+
+def make_a_movie(output_filename, source_folder, elapsed_time, last_picture_path, current_z):
     global pwm_channel
 
     if create_movie:
-        log_print("Creating video... (total height =" + str(currentZ) + "mm)")
+        log_print("Creating video... (total height =" + str(current_z) + "mm)")
         crop_table = InterpolatedArray(crop_factors)
         clear_pwm()
         pwm_channel = GPIO.PWM(led_pin, 4.0)
         pwm_channel.start(20)
         outname = snapshot_folder + "/" + output_filename + "-" + current_time_string() + ".mp4"
-        crop_factor = crop_table[currentZ]
+        crop_factor = crop_table[current_z]
         crop_options = " -vf \'crop=y=0:h=in_h*" + str(crop_factor) + "\'"
         system_command = 'ffmpeg -y -framerate 30 -pattern_type glob -i \'' + source_folder + '/*.jpg\' ' + encoding_options + crop_options + ' \'' + outname + '\''
         log_print(system_command)
@@ -188,17 +226,38 @@ def make_a_movie(output_filename, source_folder, elapsed_time, last_picture_path
         if result == 0:
             log_print("Success! New movie is " + outname)
 
-            msgBody = printer_name + " job complete: " + output_filename + "\nTotal print time: " + str(datetime.timedelta(seconds = elapsed_time)) + "\n\n"
-            send_sms(msgBody)
-            send_email(printer_name, msgBody, last_picture_path)
-            log_print(msgBody)
+            msg_body = printer_name + " job complete: " + output_filename + "\nTotal print time: " + str(datetime.timedelta(seconds = elapsed_time)) + "\n\n"
+            send_sms(msg_body)
+            send_email(printer_name, msg_body, last_picture_path)
+            log_print(msg_body)
             shutil.rmtree(source_folder)
 
         clear_pwm()
 
+
+def play_sound(sound_name):
+    while pg.mixer.music.get_busy():
+        pass
+
+    sound_path = sounds_folder + "/" + sound_name
+
+    try:
+        pg.mixer.music.load(sound_path)
+        # print("Music file {} loaded!".format(sound_path))
+    except pygame.error:
+        print("File {} not found! {}".format(sound_path, pg.get_error()))
+        return
+
+    pg.mixer.music.play()
+
+
+def sound_init():
+    pg.mixer.init(44100, -16, 2, 2048)
+    pg.mixer.music.set_volume(sound_volume)
+
+
 def firmware_monitor():
-    sock = None
-    conn = None
+    global sock, conn, printing, awaiting_baby_step
 
     while True:
         try:
@@ -216,80 +275,135 @@ def firmware_monitor():
                 conn = SimpleLineProtocol(sock)
 
                 timelapse_folder = None
-                currentLayer = -1
-                currentZ = 0
-                previousZ1 = 0
-                previousZ2 = 0
-                lastLayer = -1
+                current_layer = -1
+                current_z = 0
+                max_z = 0
+                last_baby_step = -1000.0
+                last_layer = -1
                 image_count = 0
                 gcode_filename = ''
-                startTime = time.time()
+                start_time = time.time()
+                next_status_time = start_time + 1.0
+                next_blink_time = start_time + 5.0
                 last_picture_path = ""
+                data = None
+                message = None
+                sent_error_msg = False
+                sent_pause_msg = False
 
                 while sock:
+                    current_time = time.time()
                     status = "I"
 
                     try:
-                        conn.write('M408 S4')
-                        data = conn.read_json_line()
+                        data,message = conn.read_data()
 
-                        if debugging:
-                            runTime = time.time() - startTime
-                            if runTime > 3 and image_count <= minimum_image_count:
-                                data['status'] = 'P'
-                                data['currentLayer'] = data['currentLayer'] + 1
-                                data['coords']['xyz'][2] = data['currentLayer'] * 0.2
+                        if data:
+                            if debugging:
+                                run_time = time.time() - start_time
+                                if run_time > 3 and image_count <= minimum_image_count:
+                                    data['status'] = 'P'
+                                    data['currentLayer'] = data['currentLayer'] + 1
+                                    data['coords']['xyz'][2] = data['currentLayer'] * 0.2
 
-                        status = data['status']
-                        currentLayer = data['currentLayer']
-                        previousZ2 = previousZ1
-                        previousZ1 = currentZ
-                        currentZ = data['coords']['xyz'][2]
+                            status = data['status']
+                            current_layer = data['currentLayer']
+                            current_z = data['coords']['xyz'][2]
+
+                            temp_fault = False
+                            temps = data['temps']
+                            bed_state = temps['bed']['state']
+
+                            if bed_state == 3:
+                                temp_fault = True
+                            else:
+                                heads = temps.get("heads")
+                                if heads:
+                                    states = heads.get("state")
+                                    if states:
+                                        for state in states:
+                                            if state == 3:
+                                                temp_fault = True
+                                                break
+
+                            if temp_fault:
+                                status = '*'
+
+                            if status == 'P':
+                                if current_z > 0 and current_z < max_z + 10:
+                                    max_z = current_z
+
+                                current_baby_step = float(data['params']['babystep'])
+
+                                if last_baby_step == -1000 and current_baby_step == 0:
+                                    last_baby_step = 0;
+                                elif last_baby_step != -1000 and last_baby_step != current_baby_step:
+                                    if current_baby_step > last_baby_step:
+                                        play_sound("click-down.wav")
+                                    else:
+                                        play_sound("click-up.wav")
+                                    last_baby_step = current_baby_step
+                                    awaiting_baby_step = False
+
+                        if message:
+                            if message.startsWith('sound:'):
+                                play_sound(message[6:])
+
+                        if not data and not message and current_time > next_status_time:
+                            conn.write('M408 S4')
+                            delay_time = 0.1 if awaiting_baby_step else printer_status_delay
+                            next_status_time - current_time + delay_time
 
                     except:
                         sock.close()
                         sock = None
                         conn = None
 
-                    if status == 'I' and not timelapse_folder:
+                    if status == 'I' and not timelapse_folder and current_time > next_blink_time:
                         set_active_light(True)
                         time.sleep(0.003)
                         set_active_light(False)
+                        next_blink_time = current_time + 5.0
 
                     if debugging:
                         log_print("Printer status: " + status)
-                        # log_print(data)
+                        if data:
+                            # log_print(data)
+                        if message:
+                            # log_print("Message: " + message)
 
                     if status == 'P' and not timelapse_folder:
                         conn.write('M36')
-                        fileInfo = conn.read_json_line()
+                        file_info = conn.read_json_line()
 
                         if debugging:
-                            fileInfo['fileName'] = 'SampleName'
+                            file_info['fileName'] = 'SampleName'
                         else:
-                            startTime = time.time()
+                            start_time = time.time()
 
-                        fileName = fileInfo['fileName']
-                        gcode_filename = os.path.splitext(os.path.basename(fileName))[0]
-                        current_print_folder = "images/{}-{}".format(current_time_string(), gcode_filename)
-                        timelapse_folder = os.path.expanduser(snapshot_folder)
-                        timelapse_folder = os.path.abspath(os.path.join(timelapse_folder, current_print_folder))
+                        filename = file_info['fileName']
+                        gcode_filename = os.path.splitext(os.path.basename(filename))[0]
+                        timelapse_folder = "{}/images/{}-{}".format(snapshot_folder, current_time_string(), gcode_filename)
                         os.makedirs(timelapse_folder)
                         set_active_light(True)
-                        log_print("New timelapse folder created: {}{}".format(timelapse_folder, os.path.sep))
+                        printing = True
+                        log_print("New timelapse folder created: {}".format(timelapse_folder))
                         log_print("Waiting for layer changes...")
 
                     if status == 'I' and timelapse_folder:
                         if create_movie:
                             if image_count > minimum_image_count or debugging:
-                                make_a_movie(gcode_filename, timelapse_folder, long(time.time() - startTime), last_picture_path, previousZ2)
+                                make_a_movie(gcode_filename, timelapse_folder, long(time.time() - start_time), last_picture_path, max_z)
                             else:
                                 log_print("Movie too short - canceling")
                                 shutil.rmtree(timelapse_folder)
 
                         timelapse_folder = None
-                        lastLayer = -1
+                        last_layer = -1
                         last_picture_path = ''
+                        last_baby_step = -1000.0
+                        printing = False
+                        awaiting_baby_step = False
                         if not debugging:
                             image_count = 0
                         log_print("Print finished.")
@@ -297,27 +411,45 @@ def firmware_monitor():
                     if status == 'I':
                         set_active_light(False)
 
+                    if status == 'A' or status == 'S' and not sent_pause_msg:
+                        msg_body = printer_name + ": PAUSED!\nProbable filament error."
+                        send_sms(msg_body)
+                        if debugging:
+                            log_print(msg_body)
+                        sent_pause_msg = True
+
+                    if status == '*' and not sent_error_msg:
+                        msg_body = printer_name + ": HEATER FAULT!"
+                        send_sms(msg_body)
+                        if debugging:
+                            log_print(msg_body)
+                        sent_error_msg = True
+
+                    if status == 'P' or status == 'I':
+                        sent_pause_msg = False;
+                        sent_error_msg = False;
+
                     if timelapse_folder:
-                        if currentLayer > lastLayer:
+                        if current_layer > last_layer:
                             last_picture_path = layer_changed(timelapse_folder, webcam_url)
                             image_count = image_count+1
 
                     if debugging:
-                        lastLayer = currentLayer - 1
-                        if status == 'I' or image_count > minimum_image_count:
-                            time.sleep(printer_status_delay)
-                    elif sock:
-                        lastLayer = currentLayer
+                        last_layer = current_layer - 1
                         time.sleep(printer_status_delay)
+                    elif sock:
+                        last_layer = current_layer
 
         except Exception as e:
             # log_print('ERROR', e)
             traceback.print_exc()
 
         set_active_light(False)
+        printing = False
+        awaiting_baby_step = False
 
         if sock:
-            log_print("Exception occured. Will restart after 15 seconds...")
+            log_print("Exception occured. Will restart after delay...")
             blink_error(5)
             time.sleep(10)
             sock.close()
@@ -328,6 +460,23 @@ def firmware_monitor():
             #     log_print("Unable to connect to printer. Will try again in 5 seconds...")
             time.sleep(5)
 
+
+def button_down():
+    global awaiting_baby_step
+
+    if printing:
+        awaiting_baby_step = True
+        conn.write('M290 S' + baby_step_amount)
+
+
+def button_up():
+    global awaiting_baby_step
+
+    if printing:
+        awaiting_baby_step = True
+        conn.write('M290 S-' + baby_step_amount)
+
+
 ################################################################################
 
 if __name__ == "__main__":
@@ -335,10 +484,19 @@ if __name__ == "__main__":
 
     try:
         log_print('Start Timelapse System')
+
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
+
         GPIO.setup(led_pin, GPIO.OUT)
         set_active_light(False)
+
+        sound_init()
+
+        GPIO.setup(button_down_pin, GPIO.IN, pull_up_down=GPIO.GPIO.PUD_UP)
+        GPIO.add_event_detect(button_down_pin, GPIO.FALLING, callback=button_down, bouncetime=100)
+        GPIO.setup(button_up_pin, GPIO.IN, pull_up_down=GPIO.GPIO.PUD_UP)
+        GPIO.add_event_detect(button_up_pin, GPIO.FALLING, callback=button_up, bouncetime=100)
 
         streamer = Popen(start_mjpg_streamer, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd = mjpg_streamer_folder)
 
