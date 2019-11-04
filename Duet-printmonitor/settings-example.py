bb55d540a37028fc1d3ffafcf3f0f06f5ee3957f
Randy Ubillos <randy@mac.com>
2019-11-04 14:39:53 -0800

    Renamed Sounds folder

--- Duet-printmonitor/settings-example.py
+++ Duet-printmonitor/settings-example.py
@@ -12,15 +12,14 @@ sound_volume = 1.0
 duet_host = "192.168.7.250" # address of the Duet Wifi or Duet Ethernet
 
 snapshot_folder = "/home/pi/timelapse-movies" # folder to place final movies and temporary images into
-sounds_folder = "/home/pi/sounds"
+sounds_folder = "/home/pi/Duet-printmonitor/sounds"
 
 mjpg_streamer_folder = "/home/pi/installs/mjpg-streamer/mjpg-streamer-experimental" # location of mjpg_streamer
 start_mjpg_streamer = "sudo ./mjpg_streamer -o 'output_http.so -w ./www' -i 'input_raspicam.so -x 972 -y 1296 -fps 15 -quality 95 -awb tungsten -ev -1 -mm matrix -rot 90'"
 webcam_url = "http://127.0.0.1:8080/?action=snapshot" # url for retrieving images from mjpg_streamer
 
 create_movie = True
-encoding_options = "-b:v 10M -minrate 3M -maxrate 15M -bufsize 15M -c:v h264_omx -vf format=yuv420p -preset slow -crf 18 -profile:v -bf 2 -coder 1 -threads 4 -cpu-used 0"
-
+encoding_options = "-b:v 10M -minrate 3M -maxrate 15M -bufsize 15M -c:v h264_omx -vf format=yuv420p -profile:v high"
 
 # Array of z heights and crop factors (from the top of frame) to be processed using FFMPEG
 crop_factors = ((0, 0.23), (50, 0.45), (100, 0.74), (200, 0.82), (300, 1.0))
